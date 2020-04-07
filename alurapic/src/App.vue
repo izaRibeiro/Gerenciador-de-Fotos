f7bc8adebb4d4f125440df6c3d518814687362b6
izaRibeiro <bellarpjf@gmail.com>
2020-04-07 13:49:33 -0300

    Barra de Pesquisa

--- alurapic/src/App.vue
+++ alurapic/src/App.vue
@@ -2,12 +2,14 @@
   <div class='corpo'>
     <h1 class='titulo'> {{ titulo }}</h1>
 
+    <input type='search' class='filtro' v-on:input='filtro = $event.target.value' placeholder='Pesquisar'>
+    
     <ul class='lista-fotos'>
       <li class='lista-fotos-item'
-        v-for='foto of fotos' :key='foto.titulo'>
+        v-for='foto of fotosComFiltro' :key='foto.titulo'>
         
         <meu-painel :titulo='foto.titulo'>
-          <img class="imagem-responsiva" :src='foto.url' :alt='foto.titulo'>
+          <img class='imagem-responsiva' :src='foto.url' :alt='foto.titulo'>
         </meu-painel>
         
       </li>
@@ -29,7 +31,22 @@ export default {
     return{
       titulo: 'Alurapic',
       fotos: [],
+      filtro: ''
     }
+  },
+
+  computed: {
+
+    fotosComFiltro(){
+      if(this.filtro){
+        let regra = new RegExp(this.filtro.trim(), 'i');
+
+        return this.fotos.filter(foto => regra.test(foto.titulo));
+      }else{
+        return this.fotos;
+      }
+    }
+
   },
 
   created() {
@@ -64,4 +81,14 @@ export default {
     display: inline-block;
   }
 
+  .filtro {
+    display: block;
+    width: 80%; 
+    padding: 12px 20px;
+    margin: 8px 0;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    box-sizing: border-box;
+    cursor: pointer;
+  }
 </style>
