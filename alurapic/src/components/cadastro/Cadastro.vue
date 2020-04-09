f9709a8f1209eede4df4028af67cf1074316e2b6
izaRibeiro <bellarpjf@gmail.com>
2020-04-09 10:37:34 -0300

    Tela de cadastro

--- alurapic/src/components/cadastro/Cadastro.vue
+++ alurapic/src/components/cadastro/Cadastro.vue
@@ -1,15 +1,101 @@
 <template>
-    <h1 class="titulo">Cadastro</h1>
+
+  <div>
+    <h1 class="centralizado">Cadastro</h1>
+    <h2 class="centralizado"></h2>
+
+    <form @submit.prevent="gravar()">
+      <div class="controle">
+        <label for="titulo">Título</label>
+        <input id="titulo" autocomplete="off" 
+        v-model.lazy="foto.titulo">
+      </div>
+
+      <div class="controle">
+        <label for="url">Url</label>
+        <input id="url" autocomplete="off" 
+        v-model.lazy="foto.url">
+        <imagem-responsiva v-show="foto.url" :url="foto.url" :titulo="foto.titulo"/>
+      </div>
+
+      <div class="controle">
+        <label for="descricao">Descrição</label>
+        <textarea id="descricao" autocomplete="off" 
+        v-model="foto.descricao">
+        </textarea>
+      </div>
+
+      <div class="centralizado">
+        <meu-botao texto="Salvar" tipo="submit"/>
+        <router-link to="/"><meu-botao texto="Voltar" tipo="button"/></router-link>
+      </div>
+
+    </form>
+  </div>
 </template>
 
 <script>
 
-</script>
+import ImagemResponsiva from '../shared/imagem-responsiva/ImagemResponsiva.vue'
+import Botao from '../shared/botao/Botao.vue';
+import Foto from '../../model/foto/Foto';
 
+export default {
+
+  components: {
+
+    'imagem-responsiva': ImagemResponsiva, 
+    'meu-botao': Botao
+  },
+
+  data(){
+      return {
+          foto: new Foto()
+      }
+  },
+
+  methods: {
+      gravar(){
+        console.log(this.foto);
+
+        this.foto = new Foto();
+      }
+  }
+}
+
+</script>
 <style scoped>
 
-    .cadastro {
-        text-align: center;
-    }
+  .centralizado {
+    text-align: center;
+  }
+  .controle {
+    font-size: 1.2em;
+    margin-bottom: 20px;
+    
+  }
+  .controle label {
+    display: block;
+    font-weight: bold;
+    margin-left: 100px;
+  }
+
+ .controle label + input, .controle textarea {
+    display: flex;
+    width: 80%;
+    position: relative; 
+    align-self: center;
+    padding: 12px 20px;
+    margin: 20px;
+    margin-left: 100px;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    box-sizing: border-box;
+    cursor: pointer;
+  }
+
+  .centralizado {
+    text-align: center;
+  }
 
 </style>
